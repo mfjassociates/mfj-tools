15fe9fada7aa9b9144e6993491fc2067888222a9
Mario Jauvin <mfj@earthling.net>
2019-04-15 23:00:39 -0400

    wire in apache httpclient and set logging for normal debug

---
+++ http-test/src/main/java/net/mfjassociates/tools/HttpTest.java
@@ -0,0 +1,55 @@
+package net.mfjassociates.tools;
+
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class HttpTest {
+
+	private static final int REQUEST_TIMEOUT = 30000;
+	private static final int CONNECT_TIMEOUT = 30000;
+	private static final int SOCKET_TIMEOUT = 60000;
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpTest.class);
+
+	}
+	@Bean
+	public CommandLineRunner clrunner(RestTemplate aRestTemplate) {
+		return args -> {
+			ResponseEntity<String> resp=aRestTemplate.getForEntity("https://www.google.ca", String.class);
+			System.out.println("size="+resp.getBody().length());
+		};
+	}
+	
+	@Bean
+	public RestTemplate restTemplate(HttpComponentsClientHttpRequestFactory hcchrf) {
+		return new RestTemplate(hcchrf);
+	}
+	
+	@Bean
+	public CloseableHttpClient chc() {
+		RequestConfig rConfig = RequestConfig.custom()
+				.setConnectionRequestTimeout(REQUEST_TIMEOUT)
+				.setConnectTimeout(CONNECT_TIMEOUT)
+				.setSocketTimeout(SOCKET_TIMEOUT).build();
+		return HttpClients.custom()
+				.setDefaultRequestConfig(rConfig)
+				.build();
+	}
+	@Bean
+	public HttpComponentsClientHttpRequestFactory chrf(CloseableHttpClient httpClient) {
+		HttpComponentsClientHttpRequestFactory chrf = new HttpComponentsClientHttpRequestFactory();
+		chrf.setHttpClient(httpClient);
+		return chrf;
+	}
+
+}
