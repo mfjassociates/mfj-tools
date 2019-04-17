3ca6b6c3ca2101b0948eaa4ce3dabce74644797b
Mario Jauvin <mfj@earthling.net>
2019-04-16 22:42:03 -0400

    added arguments and logging level support
    This added the --headers (default, org.apache.http is DEBUG and
    org.apache.http.wire is ERROR), --silent (info level) and --wire (debug)
    command line arguments.  Fixed bug where the arguments were not passed
    to the Application runner.  This also illustrates how to switch logging
    level at runtime.

--- http-test/src/main/java/net/mfjassociates/tools/HttpTest.java
+++ http-test/src/main/java/net/mfjassociates/tools/HttpTest.java
@@ -1,8 +1,14 @@
 package net.mfjassociates.tools;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,27 +17,73 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import ch.qos.logback.classic.Level;
+
 @SpringBootApplication
 public class HttpTest {
 
 	private static final int REQUEST_TIMEOUT = 30000;
 	private static final int CONNECT_TIMEOUT = 30000;
 	private static final int SOCKET_TIMEOUT = 60000;
+	private static final Logger logger=LoggerFactory.getLogger(HttpTest.class);
+	private static final ch.qos.logback.classic.Logger httpLogger=(ch.qos.logback.classic.Logger) LoggerFactory.getLogger("org.apache.http");
+	private static final ch.qos.logback.classic.Logger wireLogger=(ch.qos.logback.classic.Logger) LoggerFactory.getLogger("org.apache.http.wire");
+	private static final String SILENT_ARGUMENT = "silent";
+	private static final String HEADERS_ARGUMENT = "headers";
+	private static final String WIRE_ARGUMENT = "wire";
+	private static enum LOGGING_MODE {
+		silent, headers, wire;
+	}
+	private static LOGGING_MODE loggingMode = LOGGING_MODE.headers;
+	@Autowired
+	private RestTemplate restTemplate=null;
 
 	public static void main(String[] args) {
-		SpringApplication.run(HttpTest.class);
+		SpringApplication.run(HttpTest.class, args);
 
 	}
 	@Bean
-	public ApplicationRunner clrunner(RestTemplate aRestTemplate) {
+	public ApplicationRunner clrunner() {
 		return args -> {
-			System.out.println("# OptionArgs: "+args.getOptionNames().size());
-			args.getOptionNames().forEach(optionName -> {System.out.println(optionName+"="+args.getOptionValues(optionName));});
-			System.out.println("# NonOptionArgs: "+args.getNonOptionArgs().size());
-			args.getNonOptionArgs().forEach(optionArg -> {System.out.println(optionArg+"="+args.getOptionValues(optionArg));});
-			ResponseEntity<String> resp=aRestTemplate.getForEntity("https://www.google.ca", String.class);
-			System.out.println("size="+resp.getBody().length());
+			/**
+			 * [--silent|--wire|--headers] <list of urls>
+			 */
+			long mutually=Stream.of(args.containsOption(SILENT_ARGUMENT),
+					args.containsOption(HEADERS_ARGUMENT),
+					args.containsOption(WIRE_ARGUMENT))
+					.filter(b -> b).count();
+			if (mutually>1) throw new IllegalArgumentException("Only one of "+
+					Stream.of(SILENT_ARGUMENT, HEADERS_ARGUMENT, WIRE_ARGUMENT)
+					.map(a -> "--"+a)
+					.collect(Collectors.toList())
+					.toString()+" can be specified");
+			if (mutually==1) {
+				if (args.containsOption(SILENT_ARGUMENT)) loggingMode=LOGGING_MODE.silent;
+				if (args.containsOption(WIRE_ARGUMENT)) loggingMode=LOGGING_MODE.wire;
+				if (args.containsOption(HEADERS_ARGUMENT)) loggingMode=LOGGING_MODE.headers;
+			}
+			switch (loggingMode) {
+			case headers:
+				// do nothing, application.properties has the right defaults
+				break;
+			case silent:
+				wireLogger.setLevel(Level.INFO);
+				httpLogger.setLevel(Level.INFO);
+				break;
+			case wire:
+				wireLogger.setLevel(Level.DEBUG);
+				httpLogger.setLevel(Level.DEBUG);
+				break;
+
+			}
+			args.getNonOptionArgs().forEach(this::urlHandler);
+
 		};
+	}
+	
+	private void urlHandler(String url) {
+		ResponseEntity<String> resp=restTemplate.getForEntity(url, String.class);
+		logger.info("size="+resp.getBody().length());
 	}
 	
 	@Bean
