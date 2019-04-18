76af802a5a402ad3cfdc8293cb8f5dc2ac09fbe1
Mario Jauvin <mfj@earthling.net>
2019-04-18 16:21:56 -0400

    add support for proxies

--- apache-httpclient-resttemplate/src/main/java/net/mfjassociates/tools/config/HttpClientConfig.java
+++ apache-httpclient-resttemplate/src/main/java/net/mfjassociates/tools/config/HttpClientConfig.java
@@ -7,7 +7,11 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.http.HeaderElement;
 import org.apache.http.HeaderElementIterator;
+import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.NTCredentials;
+import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
@@ -15,10 +19,13 @@ import org.apache.http.conn.ConnectionKeepAliveStrategy;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
 import org.apache.http.conn.socket.PlainConnectionSocketFactory;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.WinHttpClients;
+import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicHeaderElementIterator;
 import org.apache.http.protocol.HTTP;
@@ -26,8 +33,13 @@ import org.apache.http.protocol.HttpContext;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
@@ -39,9 +51,10 @@ import org.springframework.scheduling.annotation.Scheduled;
  */
 @Configuration
 @EnableScheduling
+//@PropertySource(ignoreResourceNotFound=true, value="classpath:HttpClientConfig-${spring.profiles.active}.properties")
 public class HttpClientConfig {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(HttpClientConfig.class);
+    private static final Logger logger = LoggerFactory.getLogger(HttpClientConfig.class);
 
     // Determines the timeout in milliseconds until a connection is established.
     private static final int CONNECT_TIMEOUT = 30000;
@@ -56,20 +69,28 @@ public class HttpClientConfig {
     private static final int DEFAULT_KEEP_ALIVE_TIME_MILLIS = 20 * 1000;
     private static final int CLOSE_IDLE_CONNECTION_WAIT_TIME_SECS = 30;
 
+	public static enum PROXY_AUTHENTICATION_MODE {
+		ntlm, userPassword, direct;
+//		public static AUTHENTICATION_MODE getMode(String sMode) {
+//			return Arrays.stream(AUTHENTICATION_MODE.values()).filter(v -> sMode.contentEquals(v.name())).findFirst().get();
+//		}
+	}
+    
+    
     @Bean
     public PoolingHttpClientConnectionManager poolingConnectionManager() {
         SSLContextBuilder builder = new SSLContextBuilder();
         try {
             builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
         } catch (NoSuchAlgorithmException | KeyStoreException e) {
-            LOGGER.error("Pooling Connection Manager Initialisation failure because of " + e.getMessage(), e);
+            logger.error("Pooling Connection Manager Initialisation failure because of " + e.getMessage(), e);
         }
 
         SSLConnectionSocketFactory sslsf = null;
         try {
             sslsf = new SSLConnectionSocketFactory(builder.build());
         } catch (KeyManagementException | NoSuchAlgorithmException e) {
-            LOGGER.error("Pooling Connection Manager Initialisation failure because of " + e.getMessage(), e);
+            logger.error("Pooling Connection Manager Initialisation failure because of " + e.getMessage(), e);
         }
 
         Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder
@@ -104,20 +125,91 @@ public class HttpClientConfig {
     }
 
     @Bean
-    public CloseableHttpClient httpClient() {
+    @DependsOn("applrunner")
+    public CloseableHttpClient httpClient(@Autowired(required=false) HttpHost proxy, @Autowired(required=false) NTCredentials credential, @Qualifier("proxyAuthenticationMode") PROXY_AUTHENTICATION_MODE aProxyAuthenticationMode) {
+        HttpClientBuilder hcb=null;
+        switch (aProxyAuthenticationMode) {
+		case ntlm:
+			hcb = WinHttpClients.custom()
+					.setRoutePlanner(new DefaultProxyRoutePlanner(proxy));
+			break;
+
+		case userPassword:
+			CredentialsProvider credProv = new BasicCredentialsProvider();
+			credProv.setCredentials(new AuthScope(proxy.getHostName(), proxy.getPort()), credential);
+			hcb = HttpClients.custom()
+					.setDefaultCredentialsProvider(credProv)
+					.setRoutePlanner(new DefaultProxyRoutePlanner(proxy));
+			break;
+		
+		case direct:
+			hcb = HttpClients.custom();
+			break;
+
+		}
+        return common(hcb).build();
+    }
+
+    private HttpClientBuilder common(HttpClientBuilder httpClient) {
         RequestConfig requestConfig = RequestConfig.custom()
                 .setConnectionRequestTimeout(REQUEST_TIMEOUT)
                 .setConnectTimeout(CONNECT_TIMEOUT)
                 .setSocketTimeout(SOCKET_TIMEOUT).build();
 
-        return HttpClients.custom()
-                .setDefaultRequestConfig(requestConfig)
-                .setConnectionManager(poolingConnectionManager())
-                .setKeepAliveStrategy(connectionKeepAliveStrategy())
-                .build();
+    	return httpClient
+              .setDefaultRequestConfig(requestConfig)
+              .setConnectionManager(poolingConnectionManager())
+              .setKeepAliveStrategy(connectionKeepAliveStrategy());
     }
+    @Value("${defaultProxyUser:}")
+    private String defaultProxyUser;
+    @Value("${defaultProxyPassword:}")
+    private String defaultProxyPassword;
+    @Value("${defaultProxyDomain:}")
+    private String defaultProxyDomain;
+
+    /**
+     * You need to have a property proxyAuthenticationMode set to the value of the enumeration as the default for this application
+     * @param args
+     * @param aMode
+     * @return
+     */
+	@Bean
+	@DependsOn("applrunner")
+	public PROXY_AUTHENTICATION_MODE proxyAuthenticationMode(ApplicationArguments args, @Value("#{ T(net.mfjassociates.tools.config.HttpClientConfig.PROXY_AUTHENTICATION_MODE).${proxyAuthenticationMode} }") PROXY_AUTHENTICATION_MODE aMode) {
+		return aMode;
+	}
+
+//	@Autowired
+//    @Qualifier("proxyAuthenticationMode")
+//    private PROXY_AUTHENTICATION_MODE proxyAuthenticationMode;
     
-    @Bean
+	@Bean
+	@DependsOn("applrunner")
+	public NTCredentials ntCredential(@Qualifier("proxyAuthenticationMode") PROXY_AUTHENTICATION_MODE proxyAuthenticationMode) {
+		
+		if (logger.isDebugEnabled()) {
+			logger.debug("Default proxy credentials: user:{}, password:{}, domain:{}", defaultProxyUser, defaultProxyPassword.replaceAll(".", "*"), defaultProxyDomain);
+		}
+		NTCredentials credential = null;
+		if (proxyAuthenticationMode==PROXY_AUTHENTICATION_MODE.userPassword) credential=new NTCredentials(defaultProxyUser, defaultProxyPassword, null, defaultProxyDomain);
+		return credential;
+	}
+	@Bean
+	public PROXY_AUTHENTICATION_MODE defaultProxyAuthenticationMode() {
+		return PROXY_AUTHENTICATION_MODE.direct;
+	}
+	@Value("${defaultProxyHost:}")
+	private String defaultProxyHost=null;
+	@Value("${dfaultProxyPort:0}")
+	private int defaultProxyPort;
+
+	@Bean
+	public HttpHost proxy() {
+		return new HttpHost(defaultProxyHost, defaultProxyPort);
+	}
+
+	@Bean
     public Runnable idleConnectionMonitor(final PoolingHttpClientConnectionManager connectionManager) {
         return new Runnable() {
             @Override
@@ -125,14 +217,14 @@ public class HttpClientConfig {
             public void run() {
                 try {
                     if (connectionManager != null) {
-                        LOGGER.trace("run IdleConnectionMonitor - Closing expired and idle connections...");
+                        logger.trace("run IdleConnectionMonitor - Closing expired and idle connections...");
                         connectionManager.closeExpiredConnections();
                         connectionManager.closeIdleConnections(CLOSE_IDLE_CONNECTION_WAIT_TIME_SECS, TimeUnit.SECONDS);
                     } else {
-                        LOGGER.trace("run IdleConnectionMonitor - Http Client Connection manager is not initialised");
+                        logger.trace("run IdleConnectionMonitor - Http Client Connection manager is not initialised");
                     }
                 } catch (Exception e) {
-                    LOGGER.error("run IdleConnectionMonitor - Exception occurred. msg={}, e={}", e.getMessage(), e);
+                    logger.error("run IdleConnectionMonitor - Exception occurred. msg={}, e={}", e.getMessage(), e);
                 }
             }
         };
--- http-test/src/main/java/net/mfjassociates/tools/HttpTest.java
+++ http-test/src/main/java/net/mfjassociates/tools/HttpTest.java
@@ -1,5 +1,6 @@
 package net.mfjassociates.tools;
 
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -9,11 +10,13 @@ import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -34,10 +37,11 @@ public class HttpTest {
 	private static final String SILENT_ARGUMENT = "silent";
 	private static final String HEADERS_ARGUMENT = "headers";
 	private static final String WIRE_ARGUMENT = "wire";
+	private static final String ENV_ARGUMENT = "env";
 	private static enum LOGGING_MODE {
 		silent, headers, wire;
 	}
-	private static LOGGING_MODE loggingMode = LOGGING_MODE.headers;
+	private LOGGING_MODE defaultLoggingMode = LOGGING_MODE.headers;
 	@Autowired
 	private RestTemplate restTemplate=null;
 
@@ -46,43 +50,61 @@ public class HttpTest {
 		ConfigurableApplicationContext context = app.run(args);
 		context.close();
 	}
+	
 	@Bean
-	public ApplicationRunner clrunner() {
+	public ApplicationRunner applrunner(ApplicationArguments theArgs, ConfigurableEnvironment env) {
+		processArguments(theArgs, env);
 		return args -> {
-			/**
-			 * [--silent|--wire|--headers] <list of urls>
-			 */
-			long mutually=Stream.of(args.containsOption(SILENT_ARGUMENT),
-					args.containsOption(HEADERS_ARGUMENT),
-					args.containsOption(WIRE_ARGUMENT))
-					.filter(b -> b).count();
-			if (mutually>1) throw new IllegalArgumentException("Only one of "+
-					Stream.of(SILENT_ARGUMENT, HEADERS_ARGUMENT, WIRE_ARGUMENT)
-					.map(a -> "--"+a)
-					.collect(Collectors.toList())
-					.toString()+" can be specified");
-			if (mutually==1) {
-				if (args.containsOption(SILENT_ARGUMENT)) loggingMode=LOGGING_MODE.silent;
-				if (args.containsOption(WIRE_ARGUMENT)) loggingMode=LOGGING_MODE.wire;
-				if (args.containsOption(HEADERS_ARGUMENT)) loggingMode=LOGGING_MODE.headers;
-			}
-			switch (loggingMode) {
-			case headers:
-				// do nothing, application.properties has the right defaults
-				break;
-			case silent:
-				wireLogger.setLevel(Level.INFO);
-				httpLogger.setLevel(Level.INFO);
-				break;
-			case wire:
-				wireLogger.setLevel(Level.DEBUG);
-				httpLogger.setLevel(Level.DEBUG);
-				break;
-
-			}
 			args.getNonOptionArgs().forEach(this::urlHandler);
 
 		};
+	}
+
+	/**
+	 * This method will process the arguments and create the resulting defaults
+	 * 
+	 * [--silent|--wire|--headers] <list of urls>
+	 * you should use the spring.profiles property to specify the active profile.
+	 * the application-{profile}.properties can be used to define the proxy property
+	 * configuration
+	 * @param args
+	 * @param env TODO
+	 */
+	private void processArguments(ApplicationArguments args, ConfigurableEnvironment env) {
+		LOGGING_MODE loggingMode=defaultLoggingMode;
+		long mutually=Stream.of(args.containsOption(SILENT_ARGUMENT),
+				args.containsOption(HEADERS_ARGUMENT),
+				args.containsOption(WIRE_ARGUMENT))
+				.filter(b -> b).count();
+		if (mutually>1) throw new IllegalArgumentException("Only one of "+
+				Stream.of(SILENT_ARGUMENT, HEADERS_ARGUMENT, WIRE_ARGUMENT)
+				.map(a -> "--"+a)
+				.collect(Collectors.toList())
+				.toString()+" can be specified");
+		if (mutually==1) {
+			if (args.containsOption(SILENT_ARGUMENT)) loggingMode=LOGGING_MODE.silent;
+			if (args.containsOption(WIRE_ARGUMENT)) loggingMode=LOGGING_MODE.wire;
+			if (args.containsOption(HEADERS_ARGUMENT)) loggingMode=LOGGING_MODE.headers;
+		}
+		switch (loggingMode) {
+		case headers:
+			// do nothing, application.properties has the right defaults
+			break;
+		case silent:
+			wireLogger.setLevel(Level.INFO);
+			httpLogger.setLevel(Level.INFO);
+			break;
+		case wire:
+			wireLogger.setLevel(Level.DEBUG);
+			httpLogger.setLevel(Level.DEBUG);
+			break;
+
+		}
+		if (args.containsOption(ENV_ARGUMENT)) {
+			List<String> theEnv=args.getOptionValues(ENV_ARGUMENT);
+			if (theEnv==null || theEnv.size()!=1) throw new IllegalArgumentException("You must specify exactly one value for the --env argument");
+			env.addActiveProfile(theEnv.get(0)); // add to active profiles
+		}
 	}
 	
 	private void urlHandler(String url) {
